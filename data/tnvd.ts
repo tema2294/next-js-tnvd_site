96fa16c19412e587ce9ecc93842a39a104242529
tema2294 <tema.marinin@mail.ru>
2022-03-18 16:56:36 +0300

    add new images

--- data/tnvd.ts
+++ data/tnvd.ts
@@ -1,112 +1,116 @@
 export const tnvd_data = [
 
     {
-        img: 'https://motorstroi.ru/wa-data/public/shop/products/67/03/367/images/692/692.750.jpg',
+        img: '/img/tnvd/185.jpg',
         name: 'Тнвд-185.1111005',
         price: '96000'
     },
     {
-        img: 'https://motorstroi.ru/wa-data/public/shop/products/67/03/367/images/692/692.750.jpg',
+        img: '/img/tnvd/179.jpg',
         name: 'Тнвд-179.1111005-20 с ЭСУ',
         price: '109000'
     },
     {
-        img: 'https://motorstroi.ru/wa-data/public/shop/products/67/03/367/images/692/692.750.jpg',
+        img: '/img/tnvd/136.jpg',
         name: 'Тнвд-136.1111005-10 с ЭСУ',
         price: '90000'
     },
     {
-        img: 'https://motorstroi.ru/wa-data/public/shop/products/67/03/367/images/692/692.750.jpg',
+        img: '/img/tnvd/175-40.jpg',
         name: 'Тнвд-175.1111005-40',
         price: '72000'
     },
     {
-
+        img: '/img/tnvd/171.jpg',
         name: 'Тнвд-171.1111005-10',
         price: '69000'
     },
     {
-
+        img: '/img/tnvd/171.jpg',
         name: 'Тнвд-17.1111005-10',
         price: '69000'
     },
     {
-
+        img: '/img/tnvd/173.jpg',
         name: 'Тнвд-173.1111005-30',
         price: '72000'
     },
     {
-
+        img: '/img/tnvd/901.jpg',
         name: 'Тнвд-901.1111008-20',
         price: '78000'
     },
     {
-
+        img: '/img/tnvd/901.jpg',
         name: 'Тнвд-90.1111008-20',
         price: '47000'
     },
     {
-
+        img: '/img/tnvd/901.jpg',
         name: 'Тнвд-902.1111008-20',
         price: '47000'
     },
     {
-
+        img: '/img/tnvd/806.jpg',
         name: 'Тнвд-806.1111005-40',
         price: '44000'
     },
     {
 
+        img: '/img/tnvd/806.jpg',
         name: 'Тнвд-801.1111005-11',
         price: '41500'
     },
     {
-
+        img: '/img/tnvd/806.jpg',
         name: 'Тнвд-804.1111007-21',
         price: '41500'
     },
     {
-
+        img: '/img/tnvd/806.jpg',
         name: 'Тнвд-805.1111007-50',
         price: '41500'
     },
     {
-
+        img: '/img/tnvd/80.jpg',
         name: 'Тнвд-80.1111005-30',
         price: '31500'
     },
     {
-
+        img: '/img/tnvd/604.jpg',
         name: 'Тнвд-604.1111005-10',
         price: '38700'
     },
     {
 
+        img: '/img/tnvd/604.jpg',
         name: 'Тнвд-601.1111005',
         price: '38700'
     },
     {
-
         name: 'Тнвд-60.1111005-30',
         price: '25300'
     },
     {
 
+        img: '/img/tnvd/337.jpg',
         name: 'Тнвд-337.1111005-20',
         price: '75000'
     },
     {
-
+        img: '/img/tnvd/337.jpg',
         name: 'Тнвд-337.1111005-40',
         price: '56000'
     },
     {
 
+        img: '/img/tnvd/337.jpg',
         name: 'Тнвд-337.1111005-42',
         price: '56000'
     },
     {
 
+        img: '/img/tnvd/185.jpg',
         name: 'Тнвд-444.1111005-30',
         price: '91000'
     },
@@ -116,7 +120,6 @@ export const tnvd_data = [
         price: '59000'
     },
     {
-
         name: 'Тнвд-324.1111005-10',
         price: '65100'
     },
