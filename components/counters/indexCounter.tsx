d8cad7c977036d98c1488e9bb511e288799269b1
tema2294 <tema.marinin@mail.ru>
2022-04-05 16:59:52 +0300

    add yandex counter

--- components/counters/indexCounter.tsx
+++ components/counters/indexCounter.tsx
@@ -4,7 +4,6 @@ export const IndexCounter = () => {
     return (
         <Script id="index-counter" strategy="lazyOnload">
             {`
-                      <!-- Yandex.Metrika counter -->
            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -16,7 +15,6 @@ export const IndexCounter = () => {
                 webvisor:true,
                 ecommerce:"dataLayer"
            });
-        <!-- /Yandex.Metrika counter -->
            `}
         </Script>
     )
